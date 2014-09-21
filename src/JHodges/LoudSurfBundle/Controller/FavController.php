049835d49486c8eaacfb32f1900b33a0cb19c1d2
dev <dev>
2014-09-21 11:11:59 +0100

    add "delete fav" functionality couple of layout/nav tweeks (fav)

--- src/JHodges/LoudSurfBundle/Controller/FavController.php
+++ src/JHodges/LoudSurfBundle/Controller/FavController.php
@@ -13,7 +13,7 @@ use Echonest\Service\Echonest;
 use JHodges\LoudSurfBundle\Entity\FavSong;
 
 /**
- * @Route("/favs/", name="favs")
+ * @Route("/favs", name="favs")
 */
 class FavController extends Controller{
     /**
@@ -31,13 +31,13 @@ class FavController extends Controller{
     }
 
     /**
-     * @Route("add/{id}/{name}/", name="add_fav")
+     * @Route("/add/{id}/{name}/", name="add_fav")
      * @Template()
      */
     public function addFavAction(Request $request, $id, $name){
-    	$securityContext = $this->get('security.context');
-		$user = $securityContext->getToken()->getUser();
-		$em=$this->getDoctrine()->getManager();
+        $securityContext = $this->get('security.context');
+        $user = $securityContext->getToken()->getUser();
+        $em=$this->getDoctrine()->getManager();
         $en=$this->get('jhodges.echonest');
 
         $data= $en->query('song', 'profile', array(
@@ -45,16 +45,36 @@ class FavController extends Controller{
             'bucket'=>array('audio_summary','artist_discovery','artist_discovery_rank','artist_familiarity','artist_familiarity_rank','artist_hotttnesss','artist_hotttnesss_rank','artist_location','song_currency','song_currency_rank','song_hotttnesss','song_hotttnesss_rank','song_type','id:7digital-US','tracks')
         ));
 
-    	$fs=new FavSong();
-    	$fs->setUser($user);
-    	$fs->setName($name);
-    	$fs->setSongId($id);
+        $fs=new FavSong();
+        $fs->setUser($user);
+        $fs->setName($name);
+        $fs->setSongId($id);
         $fs->setData($data);
 
-    	$em->persist($fs);
-    	$em->flush();
+        $em->persist($fs);
+        $em->flush();
 
-    	return $this->redirect( $this->generateUrl('favs') );
+        return $this->redirect( $this->generateUrl('favs') );
     }
+
+    /**
+     * @Route("/delete/{id}/", name="delete_fav")
+     * @Template()
+     */
+    public function deleteFavAction(Request $request, $id){
+        $securityContext = $this->get('security.context');
+        $user = $securityContext->getToken()->getUser();
+
+        $em=$this->getDoctrine()->getManager();
+        $favSong=$em->getRepository('JHodgesLoudSurfBundle:FavSong')->findOneBy( array('user'=>$user, 'id'=>$id) );
+
+        if($favSong){
+            $em->remove($favSong);
+            $em->flush();
+        }
+
+        return $this->redirect( $this->generateUrl('favs') );
+    }
+
 
 }
